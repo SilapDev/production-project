8cb031577ca366b6473f6ad17e089203db452475
silapdev <silaprustamow4@gmail.com>
2023-07-17 16:58:08 +0500

    fix: router for tests

--- src/widgets/sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -2,14 +2,15 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import {Sidebar} from "./Sidebar"
 import { renderWithTranslations } from "shared/lib/tests/renderWithTranslations/renderWithTranslations"
+import { componentRender } from "shared/lib/tests/componentRender/componentRender"
 
 describe("Sidebar", ()=>{
     test("with only first param", ()=>{
-        renderWithTranslations(<Sidebar  />)
+        componentRender(<Sidebar  />)
         expect(screen.getByTestId("Sidebar")).toBeInTheDocument();
     })
     test("with only first param", ()=>{
-        renderWithTranslations(<Sidebar  />)
+        componentRender(<Sidebar  />)
         expect(screen.getByTestId("Sidebar")).toBeInTheDocument();
     })
 })
