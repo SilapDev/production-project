00c1ec681d2a4b9fdeb71ca190c318c24d832592
silapdev <silaprustamow4@gmail.com>
2023-07-25 17:52:20 +0500

    fix: unit test errors

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -1,21 +1,19 @@
-import axios from 'axios'
 import { loginByUsername } from './loginByUsername'
 import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk'
-
-jest.mock('axios')
-
-const mockedAxios = jest.mocked(axios, true)
+import { userActions } from 'entities/User'
 
 describe('loginByUsername.test', () => {
   test('success login', async () => {
     const userValue = { username: 'admin', id: '1' }
-    mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }))
 
     const thunk = new TestAsyncThunk(loginByUsername)
+    thunk.api.post.mockReturnValue(Promise.resolve({ data: userValue }))
     const result = await thunk.callThunk({ username: 'admin', password: '123' })
 
-    // expect(thunk.dispatch).toHaveBeenCalledTimes(2)
-    // expect(result.meta.requestStatus).toBe('fulfilled')
-    // expect(result.payload).toEqual(userValue)
+    expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue))
+    expect(thunk.dispatch).toHaveBeenCalledTimes(3)
+    expect(thunk.api.post).toHaveBeenCalled()
+    expect(result.meta.requestStatus).toBe('fulfilled')
+    expect(result.payload).toEqual(userValue)
   })
 })
